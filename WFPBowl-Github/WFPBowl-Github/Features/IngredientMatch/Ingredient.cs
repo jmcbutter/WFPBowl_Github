6af152d86aa49ca1531d4bb03a322b96e8e696d5
Jordan Butterfield <jmcbutter@gmail.com>
2022-04-05 19:05:26 -0500

    Create new solution to host WFPBowl on Github Pages using Blazor Webassembly.

---
+++ WFPBowl-Github/WFPBowl-Github/Features/IngredientMatch/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace WFPBowl_Github.Features
+{
+
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Rating { get; set; } = 100;
+        public string Category { get; set; } = "";
+        public List<Pair> PairedIngredients { get; set; } = new List<Pair>();
+        public bool IsSelected { get; set; } = false;
+
+        public void GetRating(List<Ingredient> selectedIngredients)
+        {
+            IngredientRatingCalculator calculator = new IngredientRatingCalculator();
+            Rating = calculator.CalculateRating(this, selectedIngredients);       
+        }
+    }
+
+    public class Pair
+    {
+        public int IngredientId { get; set; }
+        public int Strength { get; set; }
+        public int Factor { get; set; }
+    }
+}
+
+
