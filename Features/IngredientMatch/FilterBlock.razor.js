8674789d54f8f21c021e90e90446682d828a5616
Jordan Butterfield <jmcbutter@gmail.com>
2022-04-16 20:12:39 -0500

    Publish 2022-04-16-v2

---
+++ Features/IngredientMatch/FilterBlock.razor.js
@@ -0,0 +1,10 @@
+﻿export function underlineFilters(label, selected = true)
+{
+    console.log("Testing Testing Testing you idiot")
+    var element = document.getElementById(`Filter--${label}`);
+    if (selected == true) {
+        element.style.textDecoration = "underline";
+    } else {
+        element.style.textDecoration = "none";
+    }
+}
---
+++ js/quickmatch.js
@@ -0,0 +1,23 @@
+﻿underlineFilters = (labels, selected_label) =>
+{
+    for (label in labels)
+    {
+        console.log(labels)
+        console.log(`Filter--${label}`)
+        var element = document.getElementById(`Filter--${labels[label]}`);
+        element.style.textDecoration = "none";
+    }
+
+    var selected_element = document.getElementById(`Filter--${selected_label}`)
+    selected_element.style.textDecoration = "underline";
+}
+
+scrollTop = () =>
+{
+    document.getElementById("selectedIngredients").scrollIntoView(false)
+}
+
+clearSearch = () =>
+{
+    document.getElementById('ingredientSearchBar').value = '';
+}
